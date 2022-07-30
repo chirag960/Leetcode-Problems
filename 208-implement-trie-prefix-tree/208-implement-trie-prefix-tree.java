f7eff94a23d63b6913a5bf1228266083c77b9a4f
chirag960 <32039645+chirag960@users.noreply.github.com>
2022-07-30 15:23:17 -0700

    Time: 61 ms (64.12%), Space: 70.1 MB (19.82%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -0,0 +1,81 @@
+class Trie {
+    
+    class TrieNode {
+        private TrieNode[] links;
+        private final int R = 26;
+        private boolean isEnd;
+        
+        public TrieNode() {
+            links = new TrieNode[26];
+        }
+        
+        public boolean containsKey(char c){
+            return links[c - 'a']!=null;
+        }
+        
+        public TrieNode get(char c){
+            return links[c - 'a'];
+        }
+        
+        public void put(char c, TrieNode node){
+            links[c - 'a'] = node;
+        }
+        
+        public void setEnd(){
+            isEnd = true;
+        }
+        
+        public boolean getEnd(){
+            return isEnd;
+        }
+    }
+    
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for(char c : word.toCharArray()){
+            if(!node.containsKey(c)){
+                node.put(c, new TrieNode());
+            }
+            node = node.get(c);
+        }
+        node.setEnd();
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(char c : word.toCharArray()){
+            if(!node.containsKey(c))
+                return false;
+            else{
+                node = node.get(c);
+            }
+        }
+        return node.getEnd();
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for(char c : prefix.toCharArray()){
+            if(!node.containsKey(c))
+                return false;
+            else{
+                node = node.get(c);
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
