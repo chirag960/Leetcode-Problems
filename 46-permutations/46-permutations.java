75443c5e13076cc6335b4b852e9db64061fa4551
chirag960 <32039645+chirag960@users.noreply.github.com>
2022-08-19 12:29:58 -0700

    Time: 4 ms (25.12%), Space: 44.4 MB (76.23%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    List<List<Integer>> result =  new ArrayList<>();
+    
+    public List<List<Integer>> permute(int[] nums) {        
+        backtrack(new ArrayList<>(), nums);
+        return result;
+    }
+    
+    public void backtrack(List<Integer> list, int[] nums){
+        if(list.size() == nums.length){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(list.contains(nums[i])){
+                continue;
+            }
+            list.add(nums[i]);
+            backtrack(list, nums);
+            list.remove(list.size() - 1);
+        }
+    }
+}
